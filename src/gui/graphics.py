e08ad3981324ce11b366d621d95fb43778dc3d1b
TeZzo <willaschek.t@gmail.com>
2017-04-12 20:57:12 +0200

    testing GUI

---
+++ src/gui/graphics.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLCDNumber, QSlider, QApplication, QGridLayout, QPushButton)
+from PyQt5.QtCore import pyqtSlot
+
+
+class Graphics(QWidget):
+	def __init__(self):
+		super().__init__()
+
+		self.initUI()
+
+	def initUI(self):
+		grid = QGridLayout()
+		self.setLayout(grid)
+
+		names = ['Cls', 'Bck', '', 'Close',
+				 '7', '8', '9', '/',
+				 '4', '5', '6', '*',
+				 '1', '2', '3', '-',
+				 '0', '.', '=', '+']
+
+		positions = [(i, j) for i in range(5) for j in range(4)]
+
+		for position, name in zip(positions, names):
+
+			if name == '':
+				continue
+			button = QPushButton(name)
+			grid.addWidget(button, *position)
+
+		self.move(300, 150)
+		self.setWindowTitle('Calculator')
+		self.show()
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = Graphics()
+	sys.exit(app.exec_())
