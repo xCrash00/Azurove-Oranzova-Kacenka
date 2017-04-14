c11756aa2d0b34893a358ab63b33862d4454a4a0
Vojtech Randysek <randysekvojta@seznam.cz>
2017-04-14 21:12:51 +0200

    parenthesis

--- src/ui/logic.py
+++ src/ui/logic.py
@@ -6,136 +6,192 @@ global term
 term = ""
 
 def num_pressed(num):
-	global term
-	term += str(num)
-	print(term)
-	return 0
+    global term
+    term += str(num)
+    print(term)
+    return 0
 
 
 def operator_pressed(operator):
-	global term
-	term += ' ' + operator + ' '
-	print(term)
-	return 0
+    global term
+    term += ' ' + operator + ' '
+    print(term)
+    return 0
 
 
 def fact_pressed():
-	global term
-	term += ' !'
-	print(term)
-	return 0
+    global term
+    term += ' !'
+    print(term)
+    return 0
 
 
 def abs_pressed():
-	global term
-	term += ' abs'
-	print(term)
-	return 0
+    global term
+    term += ' abs'
+    print(term)
+    return 0
 
 
 def sqrt_pressed():
-	global term
-	term += ' sqrt'
-	print(term)
-	return 0
+    global term
+    term += ' sqrt'
+    print(term)
+    return 0
 
 
 def negate_pressed():
-	global term
-	term += '-'
-	print(term)
-	return 0
+    global term
+    term += '-'
+    print(term)
+    return 0
+
 
 def c_pressed():
-	global term
-	term = ''
-	return 0
+    global term
+    term = ''
+    return 0
 
-
-'''
-maze jen posledni cislo za operatorem, neumi zatim smazat operator
-'''
 
 def ce_pressed():
-	global term
-	while term[-1] != ' ':
-		term = term[:-1]
-	return 0
+    global term
+    if term[-1] == ' ':
+        term = term[:-1]
+    while term[-1] != ' ':
+        term = term[:-1]
+    if term[-1] == ' ':
+        term = term[:-1]
+    return 0
 
+
+def leftpar_pressed():
+    global term
+    term += '( '
+    print(term)
+    return 0
+
+
+def rightpar_pressed():
+    global term
+    term += ' )'
+    print(term)
+    return 0
 
 def parse():
-	parts = []
-	for item in term.split(' '):
-		parts.append(item)
-	return parts
+    parts = []
+    for item in term.split(' '):
+        parts.append(item)
+    return parts
 
 
-def get_res():
-	op_highprio = ['!', 'abs', 'sqrt']
-	op_prio = ['*', '/']
-	op = ['+', '-']
-	list = parse()
-	print(list)
-	simpler = []
-	for item in list:
-		simpler.append(item)
-		if item in op_highprio:
-			index = simpler.index(item)
-			value = simpler[index - 1]
-			if item == '!':
-				simpler[index - 1] = math_lib.fact(float(value))
-				del simpler[index]
-				print(simpler)
-			elif item == 'abs':
-				simpler[index - 1] = math_lib.abs(float(value))
-				del simpler[index]
-				print(simpler)
-			else:
-				simpler[index - 1] = math_lib.sqrt(float(value))
-				del simpler[index]
-				print(simpler)
-	simplerer = []
-	print(simpler)
-	skip = False
-	for item in simpler:
-		if skip == True:
-			skip = False
-			continue
-		simplerer.append(item)
-		if item in op_prio:
-			index = simpler.index(item)
-			index2 = simplerer.index(item)
-			value1 = simplerer[index2 - 1]
-			value2 = simpler[index + 1]
-			simplerer = simplerer[:-2]
-			if item == '*':
-				simplerer.append(math_lib.mul(float(value1), float(value2)))
-				print(simplerer)
-				skip = True
-			else:
-				simplerer.append(math_lib.div(float(value1), float(value2)))
-				print(simplerer)
-				skip = True
-	simplest = []
-	print(simplerer)
-	skip = False
-	for item in simplerer:
-		if skip == True:
-			skip = False
-			continue
-		simplest.append(item)
-		if item in op:
-			ind = simplerer.index(item)
-			ind2 = simplest.index(item)
-			value1 = simplest[ind2 - 1]
-			value2 = simplerer[ind + 1]
-			simplest = simplest[:-2]
-			if item == '+':
-				simplest.append(math_lib.add(float(value1), float(value2)))
-				print(simplest)
-				skip = True
-			elif item == '-':
-				simplest.append(math_lib.sub(float(value1), float(value2)))
-				print(simplest)
-				skip = True
-	return simplest
+def find_matching_par(src):
+    istart = []  # stack of indices of opening parentheses
+    d = {}
+    for i, c in enumerate(src):
+        if c == '(':
+            istart.append(i)
+        if c == ')':
+            try:
+                d[istart.pop()] = i
+            except IndexError:
+                print('Too many closing parentheses')
+    if istart:  # check if stack is empty afterwards
+        print('Too many opening parentheses')
+    return d
+
+
+def get_res(source):
+    print(source)
+    par = sorted(list(find_matching_par(source).items()))
+    if par == []:
+        op_highprio = ['!', 'abs', 'sqrt']
+        op_prio = ['*', '/', 'pow']
+        op = ['+', '-']
+
+        simpler = []
+        for item in source:
+            simpler.append(item)
+            if item in op_highprio:
+                index = simpler.index(item)
+                value = simpler[index - 1]
+                if item == '!':
+                    simpler[index - 1] = math_lib.fact(float(value))
+                    del simpler[index]
+                    print(simpler)
+                elif item == 'abs':
+                    simpler[index - 1] = math_lib.abs(float(value))
+                    del simpler[index]
+                    print(simpler)
+                else:
+                    simpler[index - 1] = math_lib.sqrt(float(value))
+                    del simpler[index]
+                    print(simpler)
+        simplerer = []
+        print(simpler)
+        skip = False
+        for item in simpler:
+            if skip == True:
+                skip = False
+                continue
+            simplerer.append(item)
+            if item in op_prio:
+                index = simpler.index(item)
+                index2 = simplerer.index(item)
+                value1 = simplerer[index2 - 1]
+                value2 = simpler[index + 1]
+                simplerer = simplerer[:-2]
+                if item == '*':
+                    simplerer.append(math_lib.mul(float(value1), float(value2)))
+                    print(simplerer)
+                    skip = True
+                elif item == 'pow':
+                    simplerer.append(math_lib.pow(float(value1), float(value2)))
+                    print(simplerer)
+                    skip = True
+                else:
+                    simplerer.append(math_lib.div(float(value1), float(value2)))
+                    print(simplerer)
+                    skip = True
+        simplest = []
+        print(simplerer)
+        skip = False
+        for item in simplerer:
+            if skip == True:
+                skip = False
+                continue
+            simplest.append(item)
+            if item in op:
+                ind = simplerer.index(item)
+                ind2 = simplest.index(item)
+                value1 = simplest[ind2 - 1]
+                value2 = simplerer[ind + 1]
+                simplest = simplest[:-2]
+                if item == '+':
+                    simplest.append(math_lib.add(float(value1), float(value2)))
+                    print(simplest)
+                    skip = True
+                elif item == '-':
+                    simplest.append(math_lib.sub(float(value1), float(value2)))
+                    print(simplest)
+                    skip = True
+        return simplest[0]
+    else:
+        first = par[0]
+        num1 = first[0] + 1
+        num2 = first[1]
+        source[first[0]] = get_res(source[num1:num2])
+        for a in range(num1, num2 + 1):
+            del source[num1]
+        return get_res(source)
+
+
+leftpar_pressed()
+leftpar_pressed()
+num_pressed(3)
+operator_pressed('+')
+num_pressed(2)
+rightpar_pressed()
+operator_pressed('*')
+num_pressed(6)
+rightpar_pressed()
+print(get_res(parse()))
